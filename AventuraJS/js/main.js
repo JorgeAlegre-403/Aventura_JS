594b64dabcb566ecc72890c92e7171b9f475f03e
yoryi404 <jalegrem04@iesalbarregas.es>
2025-11-25 12:36:26 +0100

    Añadir import showScene en el main.js y meter todas la clases en una carpeta aparte

--- AventuraJS/js/main.js
+++ AventuraJS/js/main.js
@@ -2,3 +2,17 @@ let cuchilla = new Producto("Cuchilla", "objects\Cuchilla.png", "1€", "Comun",
 let david = new Jugador("David", "characters\David.png")
 
 console.log(david.obtenerAtaqueTotal)
+
+
+import { showScene } from "./util.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("btn-to-scene-2")
+        .addEventListener("click", () => showScene("scene-2"));
+
+    document.getElementById("btn-to-scene-3")
+        .addEventListener("click", () => showScene("scene-3"));
+
+});
+
--- AventuraJS/js/util.js
+++ AventuraJS/js/util.js
@@ -1,6 +1,6 @@
 export function showScene(id) {
     document.querySelectorAll(".scene").forEach(
-        element => element.classList.remove("ative")
+        element => element.classList.remove("active")
     );
     document.getElementById(id).classList.add("active");
 }
